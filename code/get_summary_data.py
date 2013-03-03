78518a6f98dcc237471465b8d869952bc8cb15db
Kevin Peterson <kevin.peterson@mayo.edu>
2013-03-02 20:28:54 -0600

    Added summary data retrieval.

--- code/get_summary_data.py
+++ code/get_summary_data.py
@@ -1,1 +1,21 @@
-#
+import MySQLdb as mdb
+
+try:
+	con = mdb.connect('localhost', 'root', '')
+
+	cur = con.cursor()
+
+	with open ("research.sql", "r") as sql_file:
+		sql = sql_file.read()
+
+	cur.execute(sql)
+
+	desc = cur.description
+
+	rows = cur.fetchall()
+
+	for row in rows:
+		print row[0]
+
+finally:
+	con.close()
