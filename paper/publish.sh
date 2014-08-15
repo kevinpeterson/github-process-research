2d217ed6b7b8e2472e89a99360e65a903c100130
Kevin Peterson <kevin.peterson@mayo.edu>
2014-08-14 21:47:12 -0500

    PDF commit

--- paper/publish.sh
+++ paper/publish.sh
@@ -1,7 +1,9 @@
 latexmk main.tex -output-directory=build -pdf
-cp build/main.pdf /tmp/
-cd ..
-git checkout gh-pages
-mv /tmp/main.pdf github-process-research.pdf
-git commit -am "PDF commit"
-git checkout master
+if [[ $1 == "deploy" ]]; then
+	cp build/main.pdf /tmp/
+	cd ..
+	git checkout gh-pages
+	mv /tmp/main.pdf github-process-research.pdf
+	git commit -am "PDF commit"
+	git checkout master
+fi
