3783b322e6d7faeeff81178f7a2343ec68f52333
Kevin Peterson <kevin.peterson@mayo.edu>
2014-08-14 21:10:24 -0500

    PDF commit

--- paper/publish.sh
+++ paper/publish.sh
@@ -1,1 +1,7 @@
 latexmk main.tex -output-directory=build -pdf
+cp build/main.pdf /tmp/
+cd ..
+git checkout gh-pages
+mv /tmp/main.pdf github-process-research.pdf
+git commit -am "PDF commit"
+git checkout master
